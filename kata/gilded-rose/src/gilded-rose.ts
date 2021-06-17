e4499b38e4e7bd3908fd4efe251cfa0145b250d5
guillaume <guillaumep@sortify.tm>
2021-06-18 10:18:28 +1200

    refacto & conjured item added

--- kata/gilded-rose/src/gilded-rose.ts
+++ kata/gilded-rose/src/gilded-rose.ts
@@ -11,86 +11,122 @@ export class Item {
 }
 
 export class GildedRose {
-    items: Array<Item>;
-
-    constructor(items = [] as Array<Item>) {
-        this.items = items;
-    }
-
-    updateQuality() {
-        this.items.forEach(item => {
+    updateQuality(items = [] as Array<Item>) {
+        items.forEach(item => {
             this.updateItemQuality(item);
             
             this.upodateSellIn(item);
         })
             
-        return this.items;
+        return items;
     }
 
     updateItemQuality(item: Item) {
-        if (this.isNotAgedBrie(item) && this.isNotbackstage(item)) {
-            if (item.quality > 0) {
-                if (this.isNotSulfuras(item)) {
-                    if(this.itemExpired(item)) {
-                        item.quality = item.quality - 2
-                    } else {
-                        item.quality = item.quality - 1
-                    }
-                    
-                }
-            }
-        } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1
-                if (this.isbackstage(item)) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1
-                        }
-                    }
+        if (this.islegendaryItem(item)) {
+            return;
+        }
 
-                    if (item.sellIn < 1) {
-                        item.quality = 0;
-                    }
-                }
-            }
+        switch(true) {
+            case this.isAgedBrie(item):
+                this.updateAgedBrieItemQuality(item);
+                break
+            case this.isbackstagePasses(item):
+                this.updateBackstagePassesItemQuality(item);
+                break
+            case this.isConjured(item):
+                this.updateConjuredItemQuality(item);
+                break
+            default:
+                this.updateNormalItemQuality(item);
         }
     }
 
     upodateSellIn(item: Item) {
-        if (this.isSulfuras(item)) {
+        if (this.islegendaryItem(item)) {
             return;
         }
 
         item.sellIn -=  1;
     }
 
-    isNotAgedBrie(item: Item) {
-        return item.name != 'Aged Brie';
+    updateAgedBrieItemQuality(item: Item) {
+        this.increaseItemQualityBy(item, 1);
     }
 
-    isNotbackstage(item: Item) {
-        return item.name != 'Backstage passes to a TAFKAL80ETC concert';
+    updateBackstagePassesItemQuality(item: Item) {
+        if (item.sellIn < 1) {
+            this.decreaseItemQualityBy(item, item.quality);
+            return
+        }
+
+        if (item.sellIn < 6) {
+            this.increaseItemQualityBy(item, 3);
+            return
+        }
+
+        if (item.sellIn < 11) {
+            this.increaseItemQualityBy(item, 2);
+            return
+        }
+
+        this.increaseItemQualityBy(item, 1);
     }
 
-    isbackstage(item: Item) {
+    updateNormalItemQuality(item: Item) {
+        if(this.itemisExpired(item)) {
+            this.decreaseItemQualityBy(item, 2);
+            return;
+        }
+
+        this.decreaseItemQualityBy(item, 1);
+    }
+
+    updateConjuredItemQuality(item: Item) {
+        if(this.itemisExpired(item)) {
+            this.decreaseItemQualityBy(item, 4);
+            return;
+        }
+
+        this.decreaseItemQualityBy(item, 2);
+    }
+
+    decreaseItemQualityBy(item: Item, number: number) {
+        let quality = item.quality - number;
+
+        if (quality < 0) {
+            quality = 0;
+        }
+        
+        item.quality = quality;
+    }
+
+    increaseItemQualityBy(item: Item, number: number) {
+        let quality = item.quality + number;
+
+        if (quality > 50) {
+            quality = 50;
+        }
+        
+        item.quality = quality;
+    }
+
+    isAgedBrie(item: Item) {
+        return item.name == 'Aged Brie';
+    }
+
+    isbackstagePasses(item: Item) {
         return item.name == 'Backstage passes to a TAFKAL80ETC concert';
     }
 
-    isNotSulfuras(item: Item) {
-        return item.name != 'Sulfuras, Hand of Ragnaros';
+    islegendaryItem(item: Item) {
+        return ['Sulfuras, Hand of Ragnaros'].includes(item.name);
     }
 
-    isSulfuras(item: Item) {
-        return item.name == 'Sulfuras, Hand of Ragnaros';
-    }
-
-    itemExpired(item: Item) {
+    itemisExpired(item: Item) {
         return item.sellIn < 1;
+    }
+
+    isConjured(item: Item) {
+        return item.name == 'Conjured';
     }
 }
